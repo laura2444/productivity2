d7ffea2e694ad88d261fc24aed0dfd42caf3ffbc
Laura <lauracast142@gmail.com>
2025-03-28 19:27:56 -0500

    agregar tabs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,9 @@ export const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+  },  {
+    path: 'tab5',
+    loadComponent: () => import('./tab5/tab5.page').then( m => m.Tab5Page)
   },
+
 ];
