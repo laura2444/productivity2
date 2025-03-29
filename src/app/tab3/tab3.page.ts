adc28e7b95c5a9b128772dbd883e6cc7ded9ba4b
Carlos Rangel <carloseduardo712404@hotmail.com>
2025-03-29 14:57:21 -0500

    Subdividir html Basico Tiene la estructura inicial sin css de la pagina de subdividir tarea. Falta la estructura correspondiente a la lista de sub-tareas, no la agrege porque es mejor saber que tipo de estructura y funciones tendra ese campo, ademas de la posible implementacion de un componente.

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,13 +1,43 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonBackButton,
+  IonButtons,
+  IonButton,
+  IonList,
+  IonItem,
+  IonSelect,
+  IonSelectOption,
+  IonIcon,
+  } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { addIcons } from 'ionicons';
+import { addCircleOutline } from 'ionicons/icons';
+
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, 
+    IonBackButton,
+    IonButton,
+    IonList,
+    IonItem,
+    IonSelect,
+    IonSelectOption,
+    IonIcon,]
 })
 export class Tab3Page {
-  constructor() {}
+  constructor() {
+    addIcons({addCircleOutline})
+  }
+
+  tareaSelect(event: CustomEvent){
+    // funcion que se activa cuando se selecciona una opcion
+    console.log(`Ionic Change fired: ${event.detail.value}`)
+  }
+
+  crearTareaPage(){
+    // funcion para el redireccionamiento a crear tarea
+    console.log("crear tarea")
+  }
 }
