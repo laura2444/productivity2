e596c682bd0a6293efe0660f9cae5e587fe6ef53
Laura <lauracast142@gmail.com>
2025-03-28 22:50:22 -0500

    tab de todas las tareas

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonFab,IonFabButton,IonIcon, IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonButtons, IonButton } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { addIcons } from 'ionicons';
+import { add } from 'ionicons/icons';
+import { RouterModule } from '@angular/router';
+import {
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,IonThumbnail
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonButton, IonButtons, IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent,IonSearchbar,IonFab,IonFabButton,IonIcon, IonButtons, RouterModule,IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle,IonThumbnail],
 })
 export class Tab1Page {
-  constructor() {}
+  constructor() {
+
+    addIcons({ add });
+  }
 }
