14090acda7e3a597bb307f5b538354e768a65995
Carlos Rangel <carloseduardo712404@hotmail.com>
2025-03-31 16:18:38 -0500

    Racha axolot

--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -1,18 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonImg, IonButtons, IonBackButton, IonGrid, IonRow, IonCol, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { checkboxOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab5',
   templateUrl: './tab5.page.html',
   styleUrls: ['./tab5.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonIcon, IonCol, IonRow, IonGrid, IonBackButton, IonButtons, IonImg, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class Tab5Page implements OnInit {
 
-  constructor() { }
+  constructor() { addIcons({checkboxOutline}) }
 
   ngOnInit() {
   }
