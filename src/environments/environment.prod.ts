f7287b41ab875652333bc9f686b2758fe391d66a
Laura <lauracast142@gmail.com>
2025-10-14 17:43:36 -0500

    cambio en environments

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+// src/environments/environment.prod.ts
+export const environment = {
+  production: true,
+  // URL de tu backend en producción
+  // Cámbiala cuando despliegues el backend
+  backendUrl: 'https://productivityback.onrender.com/api'
+  
+};
